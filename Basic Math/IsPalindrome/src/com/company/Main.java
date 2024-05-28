8efb2cbd156ba82b3ff2d677b1b79dd13e57c6ca
Luchi-Chang <dmerism006@gmail.com>
2024-05-28 20:07:34 +0530

    Code for IsPallindrome check

---
+++ Basic Math/IsPalindrome/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(121) );
+    }
+
+    public static String isPalindrome(int num) {
+        if (num <= 9 && num >= 0) return num + " is Palindrome.";
+
+        int noOfDigits = (int) Math.log10(num) + 1; // TC = O(logn)
+        int leftDivisor = (int) Math.pow(10, noOfDigits - 1); // TC = O(logn)
+        int rightDivisor = 10;
+
+//TC for iterator is O(n/2). where n, is no of digits
+        for (int i = 0; i < noOfDigits / 2; i++) {
+            int leftDigit = (num / leftDivisor) % 10;
+            int rightDigit = (num % rightDivisor) / (int) Math.pow(10, i);
+
+            if (leftDigit != rightDigit) {
+                return num + " is not a Palindrome.";
+            }
+
+            leftDivisor /= 10;
+            rightDivisor *= 10;
+        }
+
+        return num + " is Palindrome.";
+    }
+}
--- Basic Math/ReverseDigitNumber/src/com/company/Main.java
+++ Basic Math/ReverseDigitNumber/src/com/company/Main.java
@@ -50,6 +50,7 @@ public class Main {
                     continue;
 
                 }
+
                 rNum =  rNum*10 +  digitVal;
 
             }
